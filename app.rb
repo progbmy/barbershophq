8abf38f31a961cfc64f47a4487c9461f61cfcfac
progbmy <skuter63@mail.ru>
2019-08-09 18:34:14 +0400

    Add index.erb

--- app.rb
+++ app.rb
@@ -7,11 +7,11 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 	class Client < ActiveRecord::Base
 	end
-	
+
 	class Barber < ActiveRecord::Base
 	end
 
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index		
 end
